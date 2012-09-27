d99f87447c500c5f16e863a0b8615f8f894f88f5
Andy Georges <andy.georges@ugent.be>
2012-09-27 14:27:25 +0200

    Added namespace_packages option.

--- lib/vsc/utils/__init__.py
+++ lib/vsc/utils/__init__.py
@@ -1,4 +1,4 @@
-#the vsc/utils namespace is used in different folders along the system
-#so explicitly declare this is also the vsc/utils namespace
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
+#the vsc namespace is used in different folders allong the system
+#so explicitly declare this is also the vsc namespace
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
