f660181f547be8da0ee1808c074c6c724addefe3
Andy Georges <andy.georges@ugent.be>
2012-10-31 10:54:28 +0100

    Find a  sublist in a list.

--- lib/vsc/utils/missing.py
+++ lib/vsc/utils/missing.py
@@ -30,3 +30,19 @@ def nub(list_):
     seen = set()
     seen_add = seen.add
     return [x for x in list_ if x not in seen and not seen_add(x)]
+
+
+def find_sublist_index(ls, sub_ls):
+    """Find the index at which the sublist sub_ls can be found in ls.
+
+    @type ls: list
+    @type sub_ls: list
+
+    @return: index of the matching location or None if no match can be made.
+    """
+    sub_length = len(sub_ls)
+    for i in xrange(len(ls)):
+        if ls[i:(i + sub_length)] == sub_ls:
+            return i
+
+    return None
