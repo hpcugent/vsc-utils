90123018333c8dd64bbcb51140a4782e213101df
stdweird <stijn.deweirdt@ugent.be>
2013-08-06 12:21:09 +0200

    fix license header to vsc-utils

--- lib/vsc/utils/nagios.py
+++ lib/vsc/utils/nagios.py
@@ -3,27 +3,27 @@
 # #
 # Copyright 2012-2013 Ghent University
 #
-# This file is part of vsc-base,
+# This file is part of vsc-utils,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
 # with support of Ghent University (http://ugent.be/hpc),
 # the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
 # the Hercules foundation (http://www.herculesstichting.be/in_English)
 # and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
 #
-# http://github.com/hpcugent/vsc-base
+# http://github.com/hpcugent/vsc-utils
 #
-# vsc-base is free software: you can redistribute it and/or modify
+# vsc-utils is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Library General Public License as
 # published by the Free Software Foundation, either version 2 of
 # the License, or (at your option) any later version.
 #
-# vsc-base is distributed in the hope that it will be useful,
+# vsc-utils is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Library General Public License for more details.
 #
 # You should have received a copy of the GNU Library General Public License
-# along with vsc-base. If not, see <http://www.gnu.org/licenses/>.
+# along with vsc-utils. If not, see <http://www.gnu.org/licenses/>.
 # #
 """
 This module provides functionality to cache and report results of script executions that can readily be
