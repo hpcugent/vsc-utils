7cbaa6b3a335dbbb0880b93dd44f57735b81c7da
Andy Georges <itkovian@gmail.com>
2013-09-05 14:42:10 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ shared_setup.SHARED_TARGET.update({
 
 PACKAGE = {
     'name': 'vsc-utils',
-    'version': '1.4.1',
+    'version': '1.4.2',
     'author': [ag, sdw],
     'maintainer': [ag, sdw],
     'packages': ['vsc', 'vsc.utils'],
