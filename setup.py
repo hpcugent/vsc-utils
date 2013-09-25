858bd6a66e1bdb29e2109737431cd0610c3dd56c
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2013-09-25 18:45:46 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ shared_setup.SHARED_TARGET.update({
 
 PACKAGE = {
     'name': 'vsc-utils',
-    'version': '1.4.3',
+    'version': '1.4.4',
     'author': [ag, sdw],
     'maintainer': [ag, sdw],
     'packages': ['vsc', 'vsc.utils'],
