23a8f522d30fcde5688085dc7ea9952800f85b4b
Jens Timmerman <jens.timmerman@ugent.be>
2013-11-05 12:09:00 +0100

    got rid of paycheck

--- test/cache.py
+++ test/cache.py
@@ -33,17 +33,29 @@ import os
 import tempfile
 import time
 import sys
-from paycheck import with_checker, irange
+import random
 from unittest import TestCase, TestLoader
 
 from vsc.utils.cache import FileCache
 
+LIST_LEN = 30  # same as in paycheck
+
+
+def get_rand_data():
+    """Returns a random dict with between 0 and LIST_LEN elements  and a random threshold"""
+    length = random.randint(0, LIST_LEN)
+    data = {random.randint(0, sys.maxint): random.randint(0, sys.maxint) for x in xrange(length)}
+    threshold = random.randint(0, sys.maxint)
+    return data, threshold
+
 
 class TestCache(TestCase):
 
-    @with_checker({int: int}, irange(0, sys.maxint))
-    def test_contents(self, data, threshold):
+    def test_contents(self):
         """Check that the contents of the cache is what is expected prior to closing it."""
+        # test with random data
+        data, threshold = get_rand_data()
+
         # create a tempfilename
         (handle, filename) = tempfile.mkstemp(dir='/tmp')
         os.unlink(filename)
@@ -60,9 +72,10 @@ class TestCache(TestCase):
             self.assertTrue(value == data[key])
             self.assertTrue(ts <= now)
 
-    @with_checker({int: int}, irange(0, sys.maxint))
-    def test_save_and_load(self, data, threshold):
+    def test_save_and_load(self):
         """Check if the loaded data is the same as the saved data."""
+        # test with random data
+        data, threshold = get_rand_data()
 
         # create a tempfilename
         (handle, filename) = tempfile.mkstemp()
