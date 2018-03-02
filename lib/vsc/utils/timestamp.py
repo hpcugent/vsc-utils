0af7d9071540b3545bd9476a1ee4d49bb9a0df06
Jens Timmerman <jens.timmerman@ugent.be>
2018-03-02 13:29:22 +0100

    fixed headers

--- lib/vsc/utils/timestamp.py
+++ lib/vsc/utils/timestamp.py
@@ -12,16 +12,17 @@
 # https://github.com/hpcugent/vsc-utils
 #
 # vsc-utils is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation v2.
+# it under the terms of the GNU Library General Public License as
+# published by the Free Software Foundation, either version 2 of
+# the License, or (at your option) any later version.
 #
 # vsc-utils is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with vsc-utils.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Library General Public License
+# along with vsc-utils. If not, see <http://www.gnu.org/licenses/>.
 #
 """Timestamp tools for cache files.
 
