2f86e25c300e7ff1559f8942b20fb5d1ab0f7852
Andy Georges <andy.georges@ugent.be>
2020-04-03 16:40:14 +0200

    bump: version to 2.1.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ import vsc.install.shared_setup as shared_setup
 from vsc.install.shared_setup import ag, sdw
 
 PACKAGE = {
-    'version': '2.0.0',
+    'version': '2.1.0',
     'author': [ag, sdw],
     'maintainer': [ag, sdw],
     'excluded_pkgs_rpm': ['vsc', 'vsc.utils'],  # vsc is default, vsc.utils is provided by vsc-base
