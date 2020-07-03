0828e93571337265352ce0aecab2e750b0dccf32
Wouter Depypere <wouter.depypere@ugent.be>
2020-07-03 10:56:14 +0200

    revert fix_cli_failure

--- lib/vsc/utils/script_tools.py
+++ lib/vsc/utils/script_tools.py
@@ -391,9 +391,7 @@ class CLI(object):
         try:
             errors = self.do(self.options.dry_run)
         except Exception as err:
-            msg = "Script failed in a horrible way: %s" % err.message
-            self.fulloptions.critical(msg)
-            self.critical_exception(msg, err)
+            self.critical_exception("Script failed in a horrible way", err)
         finally:
             self.final()
 
