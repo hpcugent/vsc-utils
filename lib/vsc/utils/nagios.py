dc587ed36a5048413c1bc2a81b2c5c15d9ca13f6
stdweird <stijn.deweirdt@ugent.be>
2021-01-20 16:17:33 +0100

    no need to close the filecache when doing a read-only operation (as close triggers a write)

--- lib/vsc/utils/nagios.py
+++ lib/vsc/utils/nagios.py
@@ -276,7 +276,6 @@ class NagiosReporter(object):
             unknown_exit("%s nagios gzipped JSON file unavailable (%s)" % (self.header, self.filename))
 
         (timestamp, ((nagios_exit_code, nagios_exit_string), nagios_message)) = nagios_cache.load('nagios')
-        nagios_cache.close()
 
         if self.threshold <= 0 or time.time() - timestamp < self.threshold:
             self.log.info("Nagios check cache file %s contents delivered: %s" % (self.filename, nagios_message))
