c1e236917b656b3bb5e20d3339652e5dacdd5fbd
Andy Georges <andy.georges@ugent.be>
2025-01-10 11:53:26 +0100

    fix: avoid trying to get from NoneType

--- lib/vsc/utils/cache.py
+++ lib/vsc/utils/cache.py
@@ -143,7 +143,13 @@ class FileCache:
 
         @returns: (timestamp, data) if there is data for the given key, None otherwise.
         """
-        return self.new_shelf.get(key, None) or self.shelf.get(key, None)
+        result = None
+        if self.new_shelf is not None:
+            result = self.new_shelf.get(key, None)
+        if result is None and self.shelf is not None:
+            return self.shelf.get(key, None)
+
+        return result
 
     def retain(self):
         """Retain non-updated data on close."""
